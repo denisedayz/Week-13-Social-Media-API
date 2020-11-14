ce3a436e9bce4bd568cb758c9388a1c2b6041884
denisedayz <69566779+denisedayz@users.noreply.github.com>
2020-11-14 15:47:56 -0700

    test

--- src/main/java/com/promineotech/socialMediaApi/controller/CommentController.java
+++ src/main/java/com/promineotech/socialMediaApi/controller/CommentController.java
@@ -15,22 +15,24 @@ import com.promineotech.socialMediaApi.service.CommentService;
 @RestController
 @RequestMapping("/users/{userId}/posts/{postId}/comments")
 public class CommentController {
-	
+
 	@Autowired
 	private CommentService service;
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Object> createComment(@RequestBody Comment comment, @PathVariable Long userId, @PathVariable Long postId) {
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Object> createComment(@RequestBody Comment comment, @PathVariable Long userId,
+			@PathVariable Long postId) {
 		try {
 			return new ResponseEntity<Object>(service.createComment(comment, userId, postId), HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<Object>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
-	
-	@RequestMapping(value = "/{commentId}", method=RequestMethod.DELETE)
+
+	@RequestMapping(value = "/{commentId}", method = RequestMethod.DELETE)
 	public ResponseEntity<Object> deleteComment(@PathVariable Long commentId) {
-		service.deleteComment(commentId);;
+		service.deleteComment(commentId);
+		;
 		return new ResponseEntity<Object>("Deleted comment with id:" + commentId, HttpStatus.OK);
 	}
 
